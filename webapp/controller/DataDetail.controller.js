3b84c3315be95c6f3d32ffc24ac27dd0e043d020
fahrialmd <a.fahrialhamdi@gmail.com>
2025-07-11 15:43:04 +0700

    Adds file upload functionality with dialog and file handling; refactors upload and download button actions

--- webapp/controller/DataDetail.controller.js
+++ webapp/controller/DataDetail.controller.js
@@ -3,9 +3,10 @@ sap.ui.define(
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
     'sap/ui/core/routing/History',
-    "sap/ui/core/Fragment"
+    "sap/ui/core/Fragment",
+    "sap/m/plugins/UploadSetwithTable",
   ],
-  function (Controller, MessageToast, History, Fragment) {
+  function (Controller, MessageToast, History, Fragment, UploadSetwithTable) {
     'use strict';
 
     return Controller.extend('rfgundemo.controller.DataDetail', {
@@ -27,6 +28,10 @@ sap.ui.define(
         this._attachInputEventDelegates();
       },
 
+      onPluginActivated: function (oEvent) {
+        this.oUploadPluginInstance = oEvent.getParameter("oPlugin");
+      },
+
       onNavBack: function () {
         var oHistory = History.getInstance();
         var sPreviousHash = oHistory.getPreviousHash();
@@ -35,8 +40,106 @@ sap.ui.define(
           window.history.go(-1);
         } else {
           var oRouter = this.getOwnerComponent().getRouter();
-          oRouter.navTo('RouteMainScreen', {}, true);
+          oRouter.navTo("RouteMainScreen", {}, true);
         }
+      },
+
+      onDownload: function (oEvent) {
+        const oContexts = this.byId("orderList").getSelectedContexts();
+        if (oContexts && oContexts.length) {
+          oContexts.forEach((oContext) =>
+            this.oUploadPluginInstance.download(oContext, true)
+          );
+        }
+      },
+
+      onUpload: async function () {
+        this.loadFragment({
+          id: "uploadFileDialog",
+          name: "rfgundemo.view.fragment.UploadFileDialog",
+          controller: this,
+        }).then((fragment) => {
+          this.dialog = fragment;
+          this.dialog.open();
+        });
+      },
+
+      onCancelPress: function () {
+        this.dialog.close();
+        this.dialog.destroy();
+      },
+
+      onUploadPress: async function () {
+        if (!this.fileContent || !this.fileName) {
+          MessageToast.show("Please select a file to upload.");
+          return;
+        }
+
+        try {
+          // * Prepare OData action bound to the upload service
+          const oModel = this.getView().getModel();
+          const oContext = this.getView().getBindingContext();
+          console.log("Binding Context:", oContext);
+
+          // const oAction = oModel.bindContext(
+          //   "/ZR_PO_VIEW/com.sap.gateway.srvd.zui_po_view.v0001.uploadExcel(...)",
+          //   oContext
+          // );
+
+          // // * Set required parameters on the action
+          // oAction.setParameter("fileName", this.fileName);
+          // oAction.setParameter("fileContent", this.fileContent);
+          // oAction.setParameter("mimeType", this.mimeType);
+          // oAction.setParameter("fileExtension", this.fileExtension);
+
+          // // * Invoke the service call
+          // oAction.execute();
+
+          // // * On success: notify user & clean up
+          // MessageToast.show(this._getText("uploadSuccess"));
+          // this.dialog.close();
+          // this.dialog.destroy();
+        } catch (error) {
+          // ! Upload failed: log error & inform user
+          console.error("Upload failed:", error);
+          MessageToast.show(this._getText("uploadFailed", [error.message]));
+        }
+      },
+
+      onFileChange: function (oEvent) {
+        // ? Retrieve selected File objects
+        const aFiles = oEvent.getParameter("files");
+        if (!aFiles || aFiles.length === 0) {
+          MessageToast.show(this._getText("noFileSelected"));
+          this.resetFileData();
+          return;
+        }
+
+        const oFile = aFiles[0];
+        this.fileName = oFile.name;
+        this.mimeType = oFile.type;
+
+        // * Derive extension from file name
+        const aNameParts = oFile.name.split(".");
+        this.fileExtension =
+          aNameParts.length > 1 ? aNameParts.pop().toLowerCase() : "";
+
+        // * Use FileReader to load as DataURL (base64)
+        const oReader = new FileReader();
+        oReader.onload = function (e) {
+          const sDataUrl = e.target.result;
+          this.fileContent = sDataUrl.split(",")[1];
+          MessageToast.show("File loaded successfully: " + oFile.name);
+        }.bind(this);
+
+        oReader.onerror = function (error) {
+          // ! File read error: log & reset data
+          console.error("File read error:", error);
+          MessageToast.show("Error reading file: " + oFile.name);
+          this.resetFileData();
+        }.bind(this);
+
+        oReader.readAsDataURL(oFile);
       },
 
       onSaveAndPostButtonPress: function () {
